620f0acfee1d142f1f1ea26d19b3bf170bd1f8fd
s-satsangi <s.satsangi@gmail.com>
2020-10-27 05:47:00 -0400

    regexp to the reg-sque

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,21 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+
+class EmailAddressParser
+
+    attr_reader :emails
+
+    def initialize(emails)
+        @emails = emails
+    end
+
+    def parse
+        emails_arr = []
+      
+        # emails_arr=emails.split(" "|", ").uniq
+        emails_arr=emails.split(/\s|,\s/).uniq
+    end
+
+end
